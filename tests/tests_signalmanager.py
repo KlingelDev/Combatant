24164a1f2538136cf7e51d2f47fa349adecced51
Karl Ruskowski <khruskowski@gmail.com>
2023-06-19 17:34:44 +0200

    Signalmanager tests

--- tests/tests_signalmanager.py
+++ tests/tests_signalmanager.py
@@ -35,8 +35,87 @@ class SignalManagerTestCase(unittest.TestCase):
 
         self.sm.connect(self, 'SigTest', test)
         self.sm.connect(self, 'SigTest', test2)
-        logging.debug('h {0!r}'.format(self.sm._registry['SigTest']._handlers))
         self.assertEqual(len(self.sm._registry['SigTest']._handlers), 2)
+
+    def test_put(self):
+        self.sm.register('SigTest', 'Test desc')
+
+        def test():
+            return 0
+
+        self.sm.connect(self, 'SigTest', test)
+        self.sm.put('SigTest')
+
+    def test_process1(self):
+        """With callback on caller"""
+        self.sm.register('SigTest', 'Test desc')
+
+        def test(a):
+            return 2
+
+        class A:
+            def __init__(self):
+                self.x = 0
+
+            def callback(self, r):
+                self.x = r
+                return self.x
+
+        a = A()
+
+        self.sm.connect(a, 'SigTest', test)
+        self.sm.put('SigTest', 1)
+        self.sm.process()
+
+        self.assertEqual(a.x, 2)
+
+    def test_process1a(self):
+        """No callback on caller"""
+        self.sm.register('SigTest', 'Test desc')
+
+        def test(a):
+            return 2
+
+        class A:
+            def __init__(self):
+                self.x = 6
+
+        a = A()
+
+        self.sm.connect(a, 'SigTest', test)
+        self.sm.put('SigTest', 1)
+        self.sm.process()
+
+        self.assertEqual(a.x, 6)
+
+    def test_process2(self):
+        self.sm.register('SigTest', 'Test desc')
+
+        def test1(a):
+            return a
+
+        def test2(a):
+            return a+2
+
+        class A:
+            def __init__(self):
+                self.x = 0
+
+            def callback(self, r):
+                self.x = r
+                return self.x
+
+        a = A()
+        b = A()
+
+        self.sm.connect(a, 'SigTest', test1)
+        self.sm.connect(b, 'SigTest', test2)
+
+        self.sm.put('SigTest', 1)
+        self.sm.process()
+
+        self.assertEqual(a.x, 1)
+        self.assertEqual(b.x, 3)
 
 if __name__ == "__main__":
     unittest.main()
